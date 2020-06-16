25cde88d063881a865bbb16a5e0408045e430ba6
jscastanos <jec.castanos@gmail.com>
2020-06-16 16:02:10 +0800

    Setup React SPA + .Net Core App

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMIS_Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SampleController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Called Get");
+        }
+    }
+}
