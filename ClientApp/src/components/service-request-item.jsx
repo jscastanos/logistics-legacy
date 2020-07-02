d671dd0149248cc22222986f7f6337decb978850
jscastanos <jec.castanos@gmail.com>
2020-07-02 15:58:40 +0800

    Refactor Service Request(main, details, item) to use React Hooks and added RecoilJS for State Management

--- ClientApp/src/components/service-request-item.jsx
+++ ClientApp/src/components/service-request-item.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 import { formatDate } from "../helpers/date-formatter";
-import { Grid, Typography, withStyles } from "@material-ui/core";
+import { Grid, Typography, makeStyles } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
 import { getServiceStatus } from "../helpers/service-status";
 
-const styles = () => ({
+const useStyles = makeStyles({
   serviceItem: {
     display: "block",
     borderRadius: "0.3125rem",
@@ -28,48 +28,38 @@ const styles = () => ({
   },
 });
 
-class ServiceRequestItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      serviceRequest: props.serviceRequest,
-    };
-  }
+const ServiceRequestItem = ({ serviceRequest }) => {
+  const classes = useStyles();
 
-  render() {
-    const { serviceRequest } = this.state;
-    const { classes } = this.props;
-
-    return (
-      <NavLink
-        to={`/service-request/${serviceRequest.ServiceReqId}`}
-        className={`${classes.serviceItem} ${getServiceStatus(
-          serviceRequest.ServiceReqStatus
-        )}`}
-      >
-        <Grid container>
-          <Grid item xs={8} md={6}>
-            <Typography variant="subtitle2" color="initial">
-              {formatDate(serviceRequest.StartDate, "D MMM")} -{" "}
-              {formatDate(serviceRequest.EndDate, "D MMM YYYY")}
-            </Typography>
-          </Grid>
-          <Grid item xs={3} md={5} className="grow textRight">
-            <Typography
-              variant="subtitle2"
-              color="initial"
-              className="textOverflow"
-            >
-              {serviceRequest.Origin} - {serviceRequest.Destination}
-            </Typography>
-          </Grid>
-          <Grid item xs={1} className="textRight">
-            <KeyboardArrowRightIcon />
-          </Grid>
+  return (
+    <NavLink
+      to={`/service-request/${serviceRequest.ServiceReqId}`}
+      className={`${classes.serviceItem} ${getServiceStatus(
+        serviceRequest.ServiceReqStatus
+      )}`}
+    >
+      <Grid container>
+        <Grid item xs={8} md={6}>
+          <Typography variant="subtitle2" color="initial">
+            {formatDate(serviceRequest.StartDate, "D MMM")} -{" "}
+            {formatDate(serviceRequest.EndDate, "D MMM YYYY")}
+          </Typography>
         </Grid>
-      </NavLink>
-    );
-  }
-}
+        <Grid item xs={3} md={5} className="grow textRight">
+          <Typography
+            variant="subtitle2"
+            color="initial"
+            className="textOverflow"
+          >
+            {serviceRequest.Origin} - {serviceRequest.Destination}
+          </Typography>
+        </Grid>
+        <Grid item xs={1} className="textRight">
+          <KeyboardArrowRightIcon />
+        </Grid>
+      </Grid>
+    </NavLink>
+  );
+};
 
-export default withStyles(styles)(ServiceRequestItem);
+export default ServiceRequestItem;
